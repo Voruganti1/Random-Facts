51d84e11b3c77f4ad32442585ee7e8d290b81791
Voruganti1 <sudhatvs@yahoo.com>
2022-09-07 13:27:21 +0100

    Updated

--- random-facts/src/App.js
+++ random-facts/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import axios from "axios";
-
+import React, { useState } from "react";
 function App() {
-  const displayFact = () => {
-    const fact = axios.get("https://uselessfacts.jsph.pl/random.json");
-    console.log(fact);
-  };
-  return <p>{displayFact}</p>;
+  const [randomFact, setRandomFact] = useState("");
+  //const displayFact = () => {
+  const fact = axios
+    .get("https://uselessfacts.jsph.pl/random.json")
+    .then((res) => {
+      setRandomFact(res.data.text);
+    })
+    .catch((x) => null);
+  console.log(fact);
+  //setRandomFact(fact);
+  //};
+  return <p>{randomFact}</p>;
 }
 
 export default App;
