b0de7c7ed87e99ef0e409b10f44c95a1f8f72b28
Voruganti1 <sudhatvs@yahoo.com>
2022-09-08 21:39:16 +0100

    with create context

--- random-facts/src/components/AppState.js
+++ random-facts/src/components/AppState.js
@@ -1,32 +1,35 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const AppContext = createContext({});
+const URL = "https://catfact.ninja/fact";
 
-const AppState = ({ children }) => {
+const AppState = (props) => {
+  const [randomFact, setRandomFact] = useState("");
+
+  async function getData() {
+    const response = await axios.get(URL);
+    setRandomFact(response.data.fact);
+  }
+
+  useEffect(() => {
+    if (!randomFact) {
+      getData();
+    }
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
         appName: "Random Facts",
         author: "Sudha",
         appVersion: 1,
+        randomFact,
       }}
     >
-      {children}
+      {props.children}
     </AppContext.Provider>
   );
 };
 
 export default AppState;
-
-//   const [randomFact, setRandomFact] = useState("");
-
-//   async function getData() {
-//     const response = await axios.get(URL);
-//     setRandomFact(response.data.fact);
-//   }
-
-//   useEffect(() => {
-//     if (!randomFact) {
-//       getData();
-//     }
-//   }, []);
